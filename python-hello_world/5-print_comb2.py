68c9cc5525a64074fc4f529f4aff0676e598034a
splendour2024 <supremasters@gmail.com>
2023-07-18 08:47:40 -0700

    hello python

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,6 @@
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:d}{:d}".format(i, j), end="")
-        if i < 8 or j < 9:
-            print(", ", end="")
-        else:
-            print()
+for i in range(100):
+    print("{:02d}".format(i), end="")
+    if i < 99:
+        print(", ", end="")
+    else:
+        print()
