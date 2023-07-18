4725f2ce8b00c87072de5c2233e9a6d991595054
splendour2024 <supremasters@gmail.com>
2023-07-18 05:57:11 -0700

    hello python

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
 word = "Holberton"
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
-print("Middle word: {}".format("bert"))
+print("Middle word: {}".format("olberto"))
