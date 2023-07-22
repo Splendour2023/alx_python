fec9e2a56439334c785c5d8a6defd5272a29fe16
splendour2024 <supremasters@gmail.com>
2023-07-22 06:12:57 -0700

    python function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,17 @@
+
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    else:
+        sequence = [0, 1]
+        while len(sequence) < n:
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+        return sequence
+
+print(fibonacci_sequence(6))  
+print(fibonacci_sequence(1))  
+print(fibonacci_sequence(0))   
+print(fibonacci_sequence(20))  
